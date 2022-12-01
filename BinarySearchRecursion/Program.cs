012dd693fc7c3caf6f00e26b5a229a62888d25ba
Andrii Serha <andrej.serga@gmail.com>
2022-12-01 14:44:32 +0200

    Initial

---
+++ BinarySearchRecursion/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BinarySearchRecursion;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int low = 0;
+        int high = array.Length - 1;
+        int item = 5;
+        Console.WriteLine(BinarySearchRecursion(low, high, item, array));
+        Console.ReadLine();
+    }
+    static int? BinarySearchRecursion(int low, int high, int item, params int[] array)
+    {
+        if(low > high)
+        {
+            return null;
+        }
+        else
+        {
+            int middle = (low + high) / 2;
+            int guess = array[middle];
+            if (guess == item)
+            {
+                return middle;
+            }
+            if (guess > item)
+            {
+                return BinarySearchRecursion(low, high - 1, item, array);
+            }
+            else
+            {
+                return BinarySearchRecursion(low + 1, high, item, array);
+            }
+        }
+    }
+}
+
